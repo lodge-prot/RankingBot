89a0703f8ba607babf6ad358a8919e9a2c9643ff
Ryuichi1208 <35625782+ryuichi1208@users.noreply.github.com>
2019-06-26 18:24:15 +0900

    Update github_trend_python.py

--- modules/github_trend_python.py
+++ modules/github_trend_python.py
@@ -1,18 +1,5 @@
-#
-# モック
-#
-
-import urllib.request, urllib.error
-from bs4 import BeautifulSoup
-
-url = "https://github.com/trending/python"
-html = urllib.request.urlopen(url)
-soup = BeautifulSoup(html, "html.parser")
-
-BASE = "https://github.com"
-rank_list = soup.find_all("h1", {"class" : "h3 lh-condensed"})
-
-for i in range(1, len(rank_list)+1):
-    repo_url = '{}{}'.format(BASE, rank_list[i-1].find("a").get("href"))
-    print('[{}] : {}'.format(i, rank_list[i-1].find("a").get_text().strip().replace(' ', '')))
-    print('({})'.format(repo_url))
+import urllib.request as ur
+from bs4 import BeautifulSoup as bs
+ba="https://github.com"
+rl=bs(ur.urlopen(ba+"/trending/python"),'lxml').find_all("h1",{"class":"h3 lh-condensed"},limit=10)
+for u in ['[{}] {}{}'.format(i,ba,rl[i-1].find("a").get("href"))for i in range(1,len(rl)+1)]: print(u)
