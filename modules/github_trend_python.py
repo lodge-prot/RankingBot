b3249b71c599f721a1d836884e5a97eecb2bb04f
Ryuichi1208 <35625782+ryuichi1208@users.noreply.github.com>
2019-07-02 11:51:12 +0900

    Update github_trend_python.py

--- modules/github_trend_python.py
+++ modules/github_trend_python.py
@@ -2,4 +2,4 @@ import urllib.request as ur
 from bs4 import BeautifulSoup as bs
 ba="https://github.com"
 rl=bs(ur.urlopen(ba+"/trending/python"),'lxml').find_all("h1",{"class":"h3 lh-condensed"},limit=10)
-for j,u in enumerate(['{}{}'.format(ba,rl[i-1].find("a").get("href"))for i in range(1,len(rl)+1)]): print(format(j+1,'02'), u)
+for j,u in enumerate([f'{ba}{rl[i-1].find("a").get("href")}'for i in range(1,len(rl)+1)]): print(f'{j+1:02}:{u}')
