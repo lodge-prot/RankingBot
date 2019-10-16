eec50b6ec4a98bdc9a8d27483a47acde5a053a7b
watanabe.ryuichi <jpz2046@linecorp.com>
2019-10-16 16:46:03 +0900

    fix ping

--- tool/ping-go.go
+++ tool/ping-go.go
@@ -1,62 +1,63 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "io"
-    "bufio"
-    "os/exec"
-    //"time"
-    "flag"
-    "strings"
-    //"reflect"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+
+	//"time"
+	"flag"
+	"strings"
+	//"reflect"
 )
 
 type HostList struct {
-    ipaddr int
-    hostname string
+	ipaddr   int
+	hostname string
 }
 
-func get_container_list_to_text(filepath string) (container_list []string){
-    f, err := os.Open(filepath)
-    if err != nil {
-	panic(err)
-    }
-    defer f.Close()
+func get_container_list_to_text(filepath string) (container_list []string) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-    reader := bufio.NewReaderSize(f, 4096)
-    for line := ""; err == nil; line, err = reader.ReadString('\n') {
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            panic(err)
-        }
-	container_list = append(container_list, line)
-    }
-    return
+	reader := bufio.NewReaderSize(f, 4096)
+	for line := ""; err == nil; line, err = reader.ReadString('\n') {
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		container_list = append(container_list, line)
+	}
+	return
 }
 
 func exec_ping(cl []string) {
-    for _, s := range cl {
-	if s == "" {
-	    continue
+	for _, s := range cl {
+		if s == "" {
+			continue
+		}
+		out, err := exec.Command("ping", strings.TrimRight(s, "\r\n"), "-c", "3").Output()
+		if err != nil {
+			fmt.Println("[FAILED] : ", s, err, out)
+			continue
+		}
+		fmt.Printf("[OK] : %s", s)
 	}
-	out, err := exec.Command("ping", strings.TrimRight(s, "\r\n"), "-c", "3").Output()
-	if err != nil {
-	    fmt.Println("[FAILED] : ", s, err, out)
-	    continue
-	}
-	fmt.Printf("[OK] : %s", s)
-    }
 }
 
 func main() {
-    fmt.Println("===== START =====")
+	fmt.Println("===== START =====")
 
-    flag.Parse()
-    var container_list []string = get_container_list_to_text(flag.Args()[0])
-    exec_ping(container_list)
+	flag.Parse()
+	var container_list []string = get_container_list_to_text(flag.Args()[0])
+	exec_ping(container_list)
 
-    fmt.Println("=====  END  =====")
+	fmt.Println("=====  END  =====")
 }
